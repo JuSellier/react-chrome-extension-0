d7fda27a065139e08e059991b4d83330242de080
JuSellier <juju.sel.mail@gmail.com>
2021-02-11 17:05:42 +0100

    removed warnings

--- src/background.js
+++ src/background.js
@@ -1,3 +1,5 @@
+// eslint-disable-next-line no-undef
 chrome.runtime.onInstalled.addListener(function () {
+  // eslint-disable-next-line no-undef
   chrome.runtime.openOptionsPage(() => console.log("options page opened"));
 });
